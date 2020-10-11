3622366547fe6e9b9ed4583461fa6291230f4808
FuntwoX <FuntwoX@users.noreply.github.com>
2020-10-11 17:09:57 +0200

    test

--- hello.sh
+++ hello.sh
@@ -1,2 +1,20 @@
-#\!/bin/sh
-echo 'hello world' 
+#!/bin/bash
+#cronjob creation
+mkdir -p /etc/periodic/${LOOK_TIME}min
+sed -i 's|#lang#|'"$LANG"'|g' /cron_subliminal_user
+
+#for each user
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    #echo "Text read from file: $line"
+    IFS=':' read -r userName string <<< "$line"
+    
+	sed -e "s/#user#/$userName/g"  /cron_subliminal_user > /etc/periodic/${LOOK_TIME}min/cron_subliminal_$userName
+    
+done < "$1"
+
+chmod -R +x /etc/periodic/
+crontab -l | { cat; echo "*/${LOOK_TIME}     *       *       *       *       run-parts /etc/periodic/${LOOK_TIME}min"; } | crontab -
+
+echo "done"
+
+crond -f -d 8
