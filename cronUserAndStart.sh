a507bc9875a2db144b165f8c18b3654b59c96423
FuntwoX <FuntwoX@users.noreply.github.com>
2020-10-11 17:53:47 +0200

    Update cronUserAndStart.sh

--- cronUserAndStart.sh
+++ cronUserAndStart.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+#cronjob creation
+mkdir -p /etc/periodic/${LOOK_TIME}min
+sed -i 's|#lang#|'"$LANG"'|g' /cron_subliminal_user
+
+#for each user
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    #echo "Text read from file: $line"
+    IFS=':' read -r userName string <<< "$line"
+    
+	sed -e "s/#user#/$userName/g"  /cron_subliminal_user > /etc/periodic/${LOOK_TIME}min/cron_subliminal_$userName
+    
+done < "$1"
+
+chmod -R +x /etc/periodic/
+crontab -l | { cat; echo "*/${LOOK_TIME}     *       *       *       *       run-parts /etc/periodic/${LOOK_TIME}min"; } | crontab -
+
+echo "done"
+
+crond -f -d 8
